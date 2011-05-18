374bc86f730bd1a18e5f36e8a1f2dd8531f71a28
Christopher Gaudig <christopher.gaudig@dfki.de>
2011-05-18 16:28:43 +0200

    added and changed the disparity image from the orogen task to this lib

--- src/dense_stereo_types.h
+++ src/dense_stereo_types.h
@@ -1,18 +1,21 @@
 #ifndef DENSE_STEREO_TYPES__H
 #define DENSE_STEREO_TYPES__H
 
+#include <base/time.h>
+
 namespace dense_stereo{
+
   struct CameraCalibration
   {
     double fx, fy, cx, cy, d0, d1, d2, d3;
   };
-  
+
   struct ExtrinsicCalibration
   {
     double tx, ty, tz;
     double rx, ry, rz;
   };
-  
+
   struct StereoCameraCalibration
   {
     int imgWidth, imgHeight;
@@ -20,12 +23,21 @@ namespace dense_stereo{
     CameraCalibration CamRight;
     ExtrinsicCalibration extrinsic;
   };
-  
+
   struct libElasConfiguration
   {
     bool postprocess_only_left;
   };
-  
+
+  struct disparity_image
+  {
+    std::vector<float> data;
+
+    StereoCameraCalibration calibration;
+
+    base::Time time;
+  };
+
 }
 
 #endif
