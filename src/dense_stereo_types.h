3a7df511433153252e3ca71ee419f9fd06fd57d4
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-05-19 13:32:59 +0200

    changed disparity image to a distance image
    this stores all the information gained from a disparity map, but
    makes the structure more generic to also be used with other types
    of 3d imaging devices like e.g. kinect or pmd cameras.

--- src/dense_stereo_types.h
+++ src/dense_stereo_types.h
@@ -4,7 +4,7 @@
 #include <base/time.h>
 #include <vector>
 
-namespace dense_stereo{
+namespace dense_stereo {
 
   struct CameraCalibration
   {
@@ -30,16 +30,42 @@ namespace dense_stereo{
     bool postprocess_only_left;
   };
 
-  struct disparity_image
+  /** 
+   * 2D array structure representing a distance image for a pinhole camera model.
+   * 
+   * The grid pixels are scaled such that (x/scale_x)+center_x = p_x are the
+   * projective plane coordinates given a grid index x. This of course applies
+   * to y as well.
+   *
+   * The data array is a row major flattened version of the image matrix,
+   * giving the distance value d of the image points.  The relation is such
+   * that for a point on the projection plane, the 3D point z can be calculated
+   * as (p_x,p_y,1)*d = z.
+   */
+  struct distance_image
   {
-    std::vector<float> data;
+    typedef float scalar;
 
-    int height;
-    int width;
-
-    StereoCameraCalibration calibration;
-
+    /// original timestamp of the camera image
     base::Time time;
+
+    /// distance values stored in row major order
+    std::vector<scalar> data;
+
+    /// height (y) value in pixels
+    uint16_t height;
+    /// width (x) value in pixels
+    uint16_t width;
+
+    /// scale value to apply to the x axis
+    scalar scale_x;
+    /// scale value to apply to the y axis
+    scalar scale_y;
+
+    /// center offset to apply to the x axis
+    scalar center_x;
+    /// center offset to apply to the y axis
+    scalar center_y;
   };
 
 }
