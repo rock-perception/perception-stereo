df51f79ed80e5e84b6f78220de36f7b249d073dc
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-10-20 19:33:43 +0200

    added ability to load calibration files from matlab.txt files

--- test/unit.cpp
+++ test/unit.cpp
@@ -7,35 +7,27 @@
 
 #include "opencv2/highgui/highgui.hpp"
 
-frame_helper::StereoCalibration getTestCalibration()
-{
-    frame_helper::StereoCalibration calib = 
-    {
-	// calibration values for test images 
-	// this is not meant to be the way to configure a calibration
-	// rather save the calibration in a file and load it dynamically 
-	// currently this works on the ruby level (e.g. see the orogen module scripts dir)
-	{280.88145,   281.69324,  320.14464,   233.86583,  -0.00601,   0.00475,   0.00069,   0.00017},
-	{284.24943,   285.02469,  320.04661,   233.68118,  -0.00492,   0.00369,   0.00033,   0.00083},
-	{-251.39129,   -0.46770,  -3.91264,   -0.00439,   -0.00603,  0.01186 }
-    };
+const std::string prefix = "test/";
+const std::string prefix_out = "build/test/";
 
-    return calib;
+frame_helper::StereoCalibration getTestCalibration( const std::string& name )
+{
+    return frame_helper::StereoCalibration::fromMatlabFile( prefix + "calib" + name + ".txt" );
 }
 
-void getTestImages( cv::Mat& left, cv::Mat& right )
+void getTestImages( const std::string& name, cv::Mat& left, cv::Mat& right )
 {
     // load the test images and perform conversion
     // to grayscale and undistort them
-    cv::Mat cleft = cv::imread( "test/left.png" );
-    cv::Mat cright = cv::imread( "test/right.png" );
+    cv::Mat cleft = cv::imread( prefix + "left" + name + ".png" );
+    cv::Mat cright = cv::imread( prefix + "right" + name + ".png" );
     cv::Mat gleft, gright;
 
     cv::cvtColor( cleft, gleft, CV_BGR2GRAY );
     cv::cvtColor( cright, gright, CV_BGR2GRAY );
 
     frame_helper::StereoCalibrationCv calib;
-    calib.setCalibration( getTestCalibration() );
+    calib.setCalibration( getTestCalibration( name ) );
     calib.setImageSize( cleft.size() );
     calib.initCv();
 
@@ -47,26 +39,26 @@ BOOST_AUTO_TEST_CASE( dense_test )
 {
     // read input images
     cv::Mat left, right;
-    getTestImages( left, right );
+    getTestImages( "", left, right );
 
     // setup dense stereo object
     stereo::DenseStereo dense;
 
     // update calibration
-    dense.setStereoCalibration( getTestCalibration(), left.size().width, left.size().height );
+    dense.setStereoCalibration( getTestCalibration(""), left.size().width, left.size().height );
 
     // process left/right frame and get disparity images
     cv::Mat ldisp, rdisp;
     dense.processFramePair( left, right, ldisp, rdisp, true );
 
-    cv::imwrite( "build/test/ldisp.png", ldisp );
-    cv::imwrite( "build/test/rdisp.png", rdisp );
+    cv::imwrite( prefix_out + "ldisp.png", ldisp );
+    cv::imwrite( prefix_out + "rdisp.png", rdisp );
 
     // convert to distance images
     dense.getDistanceImages( ldisp, rdisp );
 
-    cv::imwrite( "build/test/ldist.png", ldisp );
-    cv::imwrite( "build/test/rdist.png", rdisp );
+    cv::imwrite( prefix_out + "ldist.png", ldisp );
+    cv::imwrite( prefix_out + "rdist.png", rdisp );
 }
 
 
@@ -74,11 +66,11 @@ BOOST_AUTO_TEST_CASE( psurf_test )
 {
     // read input images
     cv::Mat left, right;
-    getTestImages( left, right );
+    getTestImages( "1", left, right );
 
     // setup dense stereo object and init calibration
     stereo::DenseStereo dense;
-    dense.setStereoCalibration( getTestCalibration(), left.size().width, left.size().height );
+    dense.setStereoCalibration( getTestCalibration("1"), left.size().width, left.size().height );
 
     // process left/right frame and get disparity images
     cv::Mat ldist, rdist;
@@ -86,7 +78,7 @@ BOOST_AUTO_TEST_CASE( psurf_test )
 
     // setup sparse stereo
     stereo::StereoFeatures sparse;
-    sparse.setCalibration( getTestCalibration() );
+    sparse.setCalibration( getTestCalibration("1") );
 
     // setup configuration
     stereo::FeatureConfiguration sparseConfig;
@@ -100,5 +92,5 @@ BOOST_AUTO_TEST_CASE( psurf_test )
     sparse.processFramePair( left, right );
 
     // write sparse stereo debug image 
-    cv::imwrite( "build/test/sparse.png", sparse.getDebugImage() );
+    cv::imwrite( prefix_out + "sparse.png", sparse.getDebugImage() );
 }
